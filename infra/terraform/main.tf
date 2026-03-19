74e143a67132a41c22bac63198b8d99e108086cd
Anson <19917864+ahs0042@users.noreply.github.com>
2026-03-18 23:43:52 -0500

    Add container metrics and Grafana dashboard work

---
+++ infra/terraform/main.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "mirage_sg" {
+  name        = "mirage-security-group"
+  description = "Allow Mirage traffic"
+
+  ingress {
+    description = "API Gateway"
+    from_port   = 8010
+    to_port     = 8010
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Node Exporter"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
